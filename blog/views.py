c246e8b9c540ca8a2358bcfcde0c3d0183ab66c4
sangmin <sangmins39@gmail.com>
2018-12-24 22:30:37 +0900

    코드 간소화

--- blog/views.py
+++ blog/views.py
@@ -71,7 +71,7 @@ def post_random_create(request):
 
 # 공개 되지 않은 post를 filter로 걸러주고 그 데이터를 삭제한다.
 def preview_remove(request):
-    preview=Post.objects.filter(published_date__isnull=True)
+    preview=Post.objects.filter(published_date__isnull=True).select_related('author')
     if len(preview) != 0:
         preview.delete()
         messages.success(request, 'Success deleted')
@@ -79,23 +79,23 @@ def preview_remove(request):
         messages.error(request, 'To delete a post, you must have one item.')
     return redirect('post_list')
 
-# 공개 되지 않은 post까지 list에 나타나면 안되니까 filter로 걸러주고
-# title의 중복을 제거한 결과를 보여줌
-def post_deduplicate(request):
-    posts=Post.objects.filter(published_date__isnull=False).order_by('published_date')
-    title=set(posts[i].title for i in range(0,len(posts)))
-    title_list=list()
-    for i in title:
-        title_list.append(posts.filter(title=i).first())
-    return render(request, 'blog/post_set_list.html', {'posts':title_list})
+# 말씀하신대로 문자열만 출력되게 변경했습니다.
+# 결과는 전과 같이 나오지만 좀 더 코드가 보기가 좋아졌습니다.
+def post_remove_duplicate_title(request):
+    posts=Post.objects.filter(published_date__isnull=False).select_related('author')
+    title=set(i.title for i in posts)
+    return render(request, 'blog/post_set_list.html', {'posts':title})
 
+# id 별로 title 필드의 글자 수 출력
 def id_and_title(request):
-    posts=Post.objects.filter(published_date__isnull=False).order_by('id')
-    title_len=dict()
-    for i in range(0,len(posts)):
-        title_len[posts[i].id]=len(posts[i].title)
-    id_title_sum=list()
-    for k,v in title_len.items():
-        id_title_sum.append(k+v)
-    id_title_sum.sort()
-    return render(request, 'blog/id_title.html', {'len':title_len, 'sum_list':id_title_sum})
+    posts=Post.objects.filter(published_date__isnull=False).select_related('author')
+    i_and_t=dict()
+    for i in posts:
+        i_and_t[i.id]=str(len(i.title))
+    return render(request, 'blog/id_and_title.html', {'len':i_and_t})
+
+# id와 title문자열의 길이의 합을 구하는 함수
+def sum_of_id_title(request):
+    posts=Post.objects.filter(published_date__isnull=False).select_related('author')
+    sum_list=list(i.id+len(i.title) for i in posts)
+    return render(request, 'blog/sum_of_id_title.html', {'sum_list':sum_list})
