1058bbbf2f629bde53d475a30006c8f553dd04ae
sangmin <sangmins39@gmail.com>
2018-12-25 23:13:02 +0900

    Comment 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    # 다른 모델에 대한 링크를 의미한다.
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # 글자 수가 제한된 텍스트를 정의할 때 사용한다.
     title = models.CharField(max_length=200)
+    # 글자 수에 제한이 없는 긴 텍스트를 위한 속성이다.
     text = models.TextField()
+    # 날짜와 시간을 의미한다.
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -15,3 +19,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    # related_name은 Post 모델에서 댓글 목록(Comment)에 접근하기 위한 이름이다.
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
